3701455c208d52a4cd4be4404311fd83a444c244
Kir.Kudryavtsev <Dragon348@mail.ru>
2020-09-04 15:19:05 +0300

    Реализонан класс Phone в соответствие условиям

--- src/ru/x5/ptoneTask/Phone.java
+++ src/ru/x5/ptoneTask/Phone.java
@@ -5,12 +5,33 @@ public class Phone {
      String model;
      int weight;
 
+     public Phone(String number, String model, int weigth){
+          this(number, model);
+          this.weight = weigth;
+     }
+     public Phone(String number, String model){
+          this.number = number;
+          this.model = model;
+     }
+     public Phone(){
+          this("+79999999999","c-01",222);
+     }
      public void recieveCall(String callerName){
           System.out.println("Звонит " + callerName);
+     }
+
+     public void recieveCall(String callerName, String number){
+          System.out.println("Звонит " + callerName);
+          System.out.println("Номер " + number);
      }
 
      public String getNumber(){
           return number;
      }
 
+     public void sendMessage(String ... numbers){
+          for(String s:numbers){
+               System.out.println("Отправлено: " + s);
+          }
+     }
 }
