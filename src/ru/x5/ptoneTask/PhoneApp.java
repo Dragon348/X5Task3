f521a65895ec76bf44c1c900c9e8d10e9f5f2c32
Kir.Kudryavtsev <Dragon348@mail.ru>
2020-09-15 09:20:37 +0300

    Внесены исправления по замечаниям

--- src/ru/x5/ptoneTask/PhoneApp.java
+++ src/ru/x5/ptoneTask/PhoneApp.java
@@ -1,22 +1,25 @@
 package ru.x5.ptoneTask;
 
 public class PhoneApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Phone[] phones = new Phone[3];
+        String[] callers = new String[3];
+        String[] numbers = new String[3];
         phones[0] = new Phone();
         phones[1] = new Phone();
         phones[2] = new Phone();
-        for (Phone phone: phones) {
-            System.out.println(phone.getNumber() + " " + phone.getModel() + " " + phone.getWeight());
-            phone.sendMessage("+79992221133", "+71234212233", "+79123219815");
+        callers[0] = "Zibulba";
+        callers[1] = "Test";
+        callers[2] = "Oleg";
+        numbers[0] = "4252";
+        numbers[1] = "425232";
+        numbers[2] = "4251112";
+        for (int i = 0; i < 3; i++) {
+            System.out.println(phones[i].getNumber() + " " + phones[i].getModel() + " " + phones[i].getWeight());
+            phones[0].recieveCall(callers[i]);
+            phones[0].recieveCall(callers[i], numbers[i]);
+            phones[i].sendMessage("+79992221133", "+71234212233", "+79123219815");
         }
-        phones[0].recieveCall("Zibulba");
-        phones[1].recieveCall("Test");
-        phones[2].recieveCall("Oleg");
-
-        phones[0].recieveCall("Zibulba", "4252");
-        phones[1].recieveCall("Test", "425232");
-        phones[2].recieveCall("Oleg", "4251112");
     }
 }
